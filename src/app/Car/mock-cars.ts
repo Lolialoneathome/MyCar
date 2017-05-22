eb26011b294d2594d1051c6aeaed59afd6679d16
Lolialoneathome <ekaterinabakuleva@gmail.com>
2017-05-22 06:48:03 +0500

    mock fix 228

--- src/app/Car/mock-cars.ts
+++ src/app/Car/mock-cars.ts
@@ -1,19 +1,19 @@
 import { Car } from './car';
 
 export const CARS: Car[] = [
-  {image: 'assets/images/car1.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car2.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car3.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car4.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'}
+  {image: 'assets/images/car1.png', price: 1235, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car2.png', price: 4325, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car3.png', price: 77654, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car4.png', price: 34534543, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'}
 ];
 
 export const STOCKED_CARS: Car[] = [
-  {image: 'assets/images/car1.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car2.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car3.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car4.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car1.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car2.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car3.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
-  {image: 'assets/images/car4.png', price: 228, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'}
+  {image: 'assets/images/car1.png', price: 5435, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car2.png', price: 534534, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car3.png', price: 543543, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car4.png', price: 534543, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car1.png', price: 225348, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car2.png', price: 225438, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car3.png', price: 4355, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'},
+  {image: 'assets/images/car4.png', price: 54353, name: 'Porsche Cayenne', description: '2011 г. / 3.6 л. / 120 000 км. / бензин'}
 ];
