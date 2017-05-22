fb8ceb7fedfc8d33883d26d17443b494bf3cecfd
Lolialoneathome <ekaterinabakuleva@gmail.com>
2017-05-22 07:06:56 +0500

    buy special beautifyer

--- src/app/Buy/Special/special-collection.component.ts
+++ src/app/Buy/Special/special-collection.component.ts
@@ -9,22 +9,22 @@ import { SpecialItemService  } from './SpecialItems/SpecialItem.service';
         <div *ngFor="let special of specials">
           <div class="row row-item">
           <div class="bordered">
-            <div class="col-md-3">
-              <img class="img-responsive" [src]=special.image alt="no icon"/>
-            </div>
-            <div class="col-md-9">
-              <div>{{special.title}}</div>
-              <div>{{special.description}} <span class='rouble'>a</span></div>
-              <div class="input-group">
-                  <div class="media">
-                   <div class="media-left media-top">
-                    <img src="assets/images/doc_icon.png" style="padding: 4px" alt="@l!" class="media-object"/>
-                    </div>
-                   <div class="media-body">
-                      <span class="media-heading product-button-text">Подробнее</span>
-                   </div>
-                </div>
-							</div>
+            <div class="col-md-12 bordered">
+              <img class="img-responsive pull-left" style="padding-right: 30px" [src]=special.image alt="no icon"/>
+              <div style="padding:50px">
+                <div>{{special.title}}</div>
+                <div>{{special.description}} <span class='rouble'>a</span></div>
+                <div class="input-group">
+                    <div class="media">
+                     <div class="media-left media-top">
+                      <img src="assets/images/doc_icon.png" style="padding: 4px" alt="@l!" class="media-object"/>
+                      </div>
+                     <div class="media-body">
+                        <span class="media-heading product-button-text">Подробнее</span>
+                     </div>
+                  </div>
+  							</div>
+              </div>
             </div>
           </div>
           </div>
