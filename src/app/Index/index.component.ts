ce23843698c1ab27bafc0274de90d82f131c9651
Lolialoneathome <ekaterinabakuleva@gmail.com>
2017-05-25 01:54:18 +0500

    navbar fix

--- src/app/Index/index.component.ts
+++ src/app/Index/index.component.ts
@@ -3,7 +3,57 @@ import { Component, Input, OnInit  } from '@angular/core';
 @Component({
     styleUrls: ['index.component.css'],
     template: `
+
+    <div class="slide-wrapper">
+
+       <div id="homepage-feature" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+             <li data-target="#homepage-feature" data-slide-to="0" class="active"></li>
+             <li data-target="#homepage-feature" data-slide-to="1"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+
+             <div class="item active" style="background-image:url(assets/images/car2.png)">
+             </div>
+
+             <div class="item" style="background-image:url(assets/images/car1.png)">
+             </div>
+
+          </div>
+          <!-- /.carousel-inner -->
+          <!-- Controls -->
+          <a class="left carousel-control" href="#homepage-feature" data-slide="prev">
+            <span class="fa fa-angle-left"></span>
+          </a>
+          <a class="right carousel-control" href="#homepage-feature" data-slide="next">
+            <span class="fa fa-angle-right"></span>
+          </a>
+       </div>
+       <!-- /#homepage-feature.carousel -->
+       <div class="red">
+         <div class="red-content">
+           <div class="header1">TOYOTA CAMRY</div>
+           <div style="padding-top:25px; padding-bottom:25px">Весенние скидки до 100 000 рублей</div>
+           <div class="jum-button">
+             <p><a class="btn btn-primary btn-custom-red  btn-primary-custom-red btn-lg" href="#" role="button">Подробнее</a></p>
+           </div>
+         </div>
+       </div>
+
+    </div>
+    <!-- /.container.slide-wrapper -->
+       <script type='text/javascript'>
+           $(document).ready(function() {
+                $('#homepage-feature').carousel({
+                    interval: 2000
+                })
+           });
+       </script>
+
     <div class="container">
+
       <div class="row" style="margin-top: 0px">
         <div class="col-md-4" style="padding-top:30px">
           <div class="header1">on-line услуги</div>
@@ -26,6 +76,9 @@ import { Component, Input, OnInit  } from '@angular/core';
         </div>
       </div>
     </div>
+
+
+
 
 
     <div class="combined-components gray-back">
