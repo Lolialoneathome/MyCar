ea2c75ce2499da1a2fdd1e72dc962f76f2ceb3e1
Lolialoneathome <ekaterinabakuleva@gmail.com>
2017-05-25 12:51:56 +0500

    navbar fix visibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,26 +13,47 @@ export class AppComponent implements OnInit {
 
   public isNavDetailsShow: boolean = false;
   public isNavDetailsHowered = false;
+  public isBuyVisible = false;
+  public isSellVisible = false;
+  public isServicesVisible = false;
+  public isAboutVisible = false;
   mouseEnter(div : string){
         console.log("mouse enter : " + div);
         this.isNavDetailsShow = true;
+        switch (div)
+        {
+         case "Buy":
+           this.isBuyVisible = true;
+           break;
+         case "Sell":
+           this.isSellVisible = true;
+           break;
+         case "Services":
+           this.isServicesVisible = true;
+           break;
+         case "About":
+           this.isAboutVisible = true;
+           break;
+         }
      }
 
      mouseLeave(div : string){
-       console.log('mouse leave :' + div);
-       //if (!this.isNavDetailsHowered)
-        //this.isNavDetailsShow = false;
+
      }
 
      mouseEnterDetailedNav(div : string){
-       console.log('mouse leave :' + div);
+
        this.isNavDetailsHowered = true;
      }
 
      mouseLeaveDetailedNav(div : string){
-       console.log('mouse leave :' + div);
+
        this.isNavDetailsShow = false;
        this.isNavDetailsHowered = false;
+       this.isBuyVisible = false;
+       this.isSellVisible = false;
+       this.isServicesVisible = false;
+       this.isAboutVisible = false;
      }
 
      constructor(public router: Router) {}
